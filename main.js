1a602499e3769193a25b46f2d777f930c1749cc7
Julian901516 <81827402+Julian901516@users.noreply.github.com>
2021-07-21 14:48:14 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -36,3 +36,74 @@ function new_image(get_image){
         canvas.add(block_image_object)
     })
 }
+window.addEventListener("keydown",my_keydown)
+function my_keydown(e){
+    keyPressed = e.keyCode
+    console.log(keyPressed)
+    if (e.shiftKey==true && keyPressed=="80"){
+        console.log("P & shift pressed together")
+        block_image_width +=10
+        block_image_height +=10
+        document.getElementById("current_width").innerHTML = block_image_width
+        document.getElementById("current_height").innerHTML = block_image_height
+    }
+    if (e.shiftKey==true && keyPressed=="77"){
+        console.log("M & shift pressed together")
+        block_image_width -=10
+        block_image_height -=10
+        document.getElementById("current_width").innerHTML = block_image_width
+        document.getElementById("current_height").innerHTML = block_image_height
+    }
+    if (keyPressed=="38"){
+        Up()
+        console.log("up arrow is pressed")
+    }
+    if (keyPressed=="40"){
+        Down()
+        console.log("down arrow is pressed")
+    }
+    if (keyPressed=="37"){
+        Left()
+        console.log("left arrow is pressed")
+    }
+    if (keyPressed=="39"){
+        Right()
+        console.log("right arrow is pressed")
+    }
+    if (keyPressed=="84"){
+        new_image("trunk.jpg")
+        console.log("t is pressed")
+    }
+    if (keyPressed=="89"){
+        new_image("yellow_wall.png")
+        console.log("y is pressed")
+    }
+    if (keyPressed=="87"){
+        new_image("wall.jpg")
+        console.log("w is pressed")
+    }
+    if (keyPressed=="85"){
+        new_image("unique.png")
+        console.log("u is pressed")
+    }
+    if (keyPressed=="82"){
+        new_image("roof.jpg")
+        console.log("r is pressed")
+    }
+    if (keyPressed=="76"){
+        new_image("light_green.png")
+        console.log("l is pressed")
+    }
+    if (keyPressed=="71"){
+        new_image("ground.png")
+        console.log("g is pressed")
+    }
+    if (keyPressed=="68"){
+        new_image("dark_green.png")
+        console.log("d is pressed")
+    }
+    if (keyPressed=="67"){
+        new_image("cloud.jpg")
+        console.log("c is pressed")
+    }
+}
